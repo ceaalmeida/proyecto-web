80b6898d7072cc3f80d09b0dc02fe759a88b7c4e
ceaalmeida <128449714+ceaalmeida@users.noreply.github.com>
2024-10-24 03:31:35 -0400

    cositas cambiadas

--- src/app/api/animal/animal.service.js
+++ src/app/api/animal/animal.service.js
@@ -1,12 +1,37 @@
+class AnimalService {
+  async getAllAnimal() {
+    const animales = await fetch("http://localhost:3000/Animal/", {
+      method: "GET",
+    });
+    const data = await animales.json();
+    return data;
+  }
+  async updateAnimal(id, animal) {
+    const animales = await fetch(`http://localhost:3000/Animal/${id}`, {
+      method: "PUT",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(animal),
+    });
+    return await Response.json;
+  }
+  async createAnimal(animal) {
+    //window.alert(`Dsde la api ${animal.Precio_Adopción}`)
+    const animals = await fetch("http://localhost:3000/Animal/", {     
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(animal),
+    });
 
-
-
-class AnimalService{
-
-    async getAllAnimal(){
-        const animales= await fetch("http://localhost:3000/Animal/",{ method:"GET"});
-        const data = await animales.json();
-        return data;
-    }
+  }
+  async deleteAnimal(id){
+    const dele= await fetch(`http://localhost:3000/Animal/${id}`,{
+        method: "DELETE",
+    })
+    // if (!dele.ok) {
+    //     window.alert("No se pudo eliminar el animal.");
+    // } else {
+    //     window.alert("Animal eliminado exitosamente.");
+    // }
+  }
 }
-export default new AnimalService()
+export default new AnimalService();

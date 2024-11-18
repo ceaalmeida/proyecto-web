fa0bea5560fbe065a0591f40185714480b676feb
ceaalmeida <128449714+ceaalmeida@users.noreply.github.com>
2024-11-18 02:04:07 -0500

    exportar pdf en mis reportes

--- src/app/home/cargar/ingresodonacionadopcion/page.js
+++ src/app/home/cargar/ingresodonacionadopcion/page.js
@@ -1,11 +1,26 @@
 import { useEffect, useState } from "react";
 import { useSession } from "next-auth/react";
 import ReportService from "../../../api/reporte/reporte.service";
-import { Paper, Table, TableHead, TableRow, TableCell, TableBody, TableContainer } from "@mui/material";
+import { Paper, Table, TableHead, TableRow, TableCell, TableBody, TableContainer,Button,CircularProgress } from "@mui/material";
+import AddIcon from "@mui/icons-material/Add";
+import jsPDF from "jspdf";
+import "jspdf-autotable";
 
+const row=[  
+  { "key": "nombre_animal", "label": "Nombre del Animal" },
+  { "key": "especie", "label": "Especie" },
+  { "key": "raza", "label": "Raza" },
+  { "key": "edad", "label": "Edad" },
+  { "key": "costo_total_mantenimiento", "label": "Costo Total Mantenimiento" },
+  { "key": "precio_total_adopcion", "label": "Precio Total Adopción" },
+  { "key": "donaciones_recibidas", "label": "Donaciones Recibidas" },
+  { "key": "monto_total_ingresos", "label": "Monto Total Ingresos" }
+]
 export default function TablePIAD() {
   const { data: session } = useSession();
   const [data, setData] = useState([]);
+  const [exportando, setExportando] = useState(false);
+  const titulo="Ingreso Donacion Acopcion"
 
   useEffect(() => {
     const fetchData = async () => {
@@ -20,20 +35,43 @@ export default function TablePIAD() {
     fetchData();
   }, [session]);
 
+  const exportarPDF = () => {
+    setExportando(true);
+    const doc = new jsPDF("portrait", "pt", "A4");
+    const marginLeft = 40;
+    const title = `${titulo} Report`;
+
+    // Configura las cabeceras de la tabla
+    const headers = [row.map((col) => col.label)];
+
+    // Mapea los datos para llenar la tabla en PDF
+    const dataRows = data.map((item) => row.map((col) => item[col.key]));
+
+    doc.setFontSize(15);
+    doc.text(title, marginLeft, 40);
+
+    doc.autoTable({
+      startY: 50,
+      head: headers,
+      body: dataRows,
+      margin: { left: marginLeft },
+    });
+
+    doc.save(`${titulo}_report.pdf`);
+    setExportando(false);
+  };
+
+
   return (
     <Paper sx={{ width: "95%", overflow: "hidden", p: 2 }}>
       <TableContainer component={Paper} sx={{ overflowX: "auto" }}>
         <Table sx={{ minWidth: 650 }} aria-label="simple table">
           <TableHead>
             <TableRow>
-              <TableCell>Nombre del Animal</TableCell>
-              <TableCell>Especie</TableCell>
-              <TableCell>Raza</TableCell>
-              <TableCell>Edad</TableCell>
-              <TableCell>Costo Total Mantenimiento</TableCell>
-              <TableCell>Precio Total Adopción</TableCell>
-              <TableCell>Donaciones Recibidas</TableCell>
-              <TableCell>Monto Total Ingresos</TableCell>
+            {row.map((ele)=>(
+              <TableCell key={ele.key}>{ele.label}</TableCell>
+            ))}
+              
             </TableRow>
           </TableHead>
           <TableBody>
@@ -52,6 +90,14 @@ export default function TablePIAD() {
           </TableBody>
         </Table>
       </TableContainer>
+      <Button
+        variant="contained"
+        startIcon={<AddIcon />}
+        onClick={exportarPDF}
+        sx={{ mt: 2 }}
+      >
+        {exportando ? <CircularProgress /> : "Exportar a PDF"}
+      </Button>
     </Paper>
   );
 }
